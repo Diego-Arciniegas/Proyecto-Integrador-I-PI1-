1d17aec54bc9dbfea992a4828d093ae5c19edc9e
diegomated <diegocardwr@gmail.com>
2022-08-19 09:44:13 -0500

    api main file

---
+++ api/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config({path: '../.env'});
+const express = require('express');
+const morgan = require('morgan');
+const bp = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(bp.urlencoded({extended: true}));
+
+const router = require('./routes/routes.js');
+app.use(router);
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Listen on port ${process.env.PORT}`);
+});
+
