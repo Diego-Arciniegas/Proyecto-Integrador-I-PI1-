f308cec62fdee38329dc06cfc02d45a3077fae10
diegomated <diegocardwr@gmail.com>
2022-10-04 09:09:30 -0500

    configuracion de las rutas

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -7,18 +7,17 @@ router.route('/auth').post(c.users.auth);
 
 router.route('/users').get(c.users.getUser);
 router.route('/users/:id_user').get(c.users.getUser);
-router.route('/users/:id_user/address').post(c.address.addAddress);
+router.route('/users/:id_user/address').post(c.address.addAddressUser);
 router.route('/users/:id_user/address').get(c.address.getAddress);
 router.route('/users/:id_user/address').put(c.address.editAddress);
 router.route('/users/:id_user/address').delete(c.address.deleteAddress);
 router.route('/users/:id_user/shopping_cart').post(c.shoppingCart.addShoppingCart);
-router.route('/users/:id_user/shopping_cart/order').post(c.orders.addOrderFromSC);
+router.route('/users/:id_user/shopping_cart/order').post(c.orders.addOrder);
 router.route('/users/:id_user/shopping_cart').get(c.shoppingCart.getShoppingCart);
 router.route('/users/:id_user/shopping_cart').delete(c.shoppingCart.deleteShoppingCart);
 router.route('/users/:id_user/shopping_cart/accessories').get(c.shoppingCart.getShoppingCart);
 router.route('/users/:id_user/shopping_cart/accessories/:id_accessory').post(c.shoppingCart.addAccessory);
 router.route('/users/:id_user/shopping_cart/accessories/:id_accessory').delete(c.shoppingCart.deleteAccesory);
-router.route('/users/:id_user/orders').post(c.orders.addOrder);
 router.route('/users/:id_user/orders').get(c.orders.getOrder);
 router.route('/users/:id_user/orders/:id_order').get(c.orders.getOneOrder);
 router.route('/users/:id_user/orders/:id_order').delete(c.orders.deleteOrderFromUser);
@@ -31,7 +30,7 @@ router.route('/business').get(c.business.getBusiness);
 router.route('/business/:id_business').get(c.business.getBusiness);
 router.route('/business/:id_business').put(c.business.editBusiness);
 router.route('/business/:id_business').delete(c.business.deleteBusiness);
-router.route('/business/:id_business/address').post(c.address.addAddress);
+router.route('/business/:id_business/address').post(c.address.addAddressBusiness);
 router.route('/business/:id_business/address/:id_address').get(c.address.getAddress);
 router.route('/business/:id_business/address/:id_address').put(c.address.editAddress);
 router.route('/business/:id_business/address/:id_address').delete(c.address.deleteAddress);
@@ -46,8 +45,6 @@ router.route('/accessories/:id_accessory').delete(c.accessories.deleteAccessory)
 router.route('/orders').get(c.orders.getOrder);
 router.route('/orders/:id_order').get(c.orders.getOneOrder);
 router.route('/orders/:id_order').delete(c.orders.deleteOrder);
-
-router.route('/test/:id_user').get(c.accessories.test);
 
 module.exports = router;
 
