cabfdf0a43ca39118f46adfca80e76d390958ee2
diegomated <diegocardwr@gmail.com>
2022-10-20 13:41:40 -0500

    Router react update

--- fronent/src/router/Router.js
+++ fronent/src/router/Router.js
@@ -1,9 +1,11 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, HashRouter, Link} from 'react-router-dom';
 
 import Login from '../components/auth/login/Login.js';
 import Register from '../components/auth/register/Register.js';
 
 import Home from '../components/home/Home.js';
+
+import Catalogo from '../components/catalogo/Catalogo.js';
 
 function Router(){
     return(
@@ -12,6 +14,7 @@ function Router(){
                 <Route exact path='/' element={<Home/>}/>
                 <Route path='/login' element={<Login/>}/>
                 <Route path='/register' element={<Register/>}/>
+                <Route path='/catalogo' element={<Catalogo/>}/>
             </Routes>
         </BrowserRouter>
     )
