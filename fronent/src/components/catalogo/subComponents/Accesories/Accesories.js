5f77b020154b73fd5b04136c90b80d29df1874f0
diegomated <diegocardwr@gmail.com>
2022-10-20 13:37:50 -0500

    Accesories component

---
+++ fronent/src/components/catalogo/subComponents/Accesories/Accesories.js
@@ -0,0 +1,21 @@
+
+//import './Accesories.css';
+import Accesory from '../Accesory/Accesory.js';
+
+function Accesories(){
+    return(
+        <div className="row table-accesories">
+            <div className="row accesories">
+                
+            </div>
+            <div className="row accesories">
+                
+            </div>
+            <div className="row accesories">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Accesories;
