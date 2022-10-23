c4cd238eccfb7e282eb85c99171226dbb9f8e080
diegomated <diegocardwr@gmail.com>
2022-10-23 17:14:21 -0500

    router update

--- fronent/src/router/Router.js
+++ fronent/src/router/Router.js
@@ -8,6 +8,9 @@ import Home from '../components/home/Home.js';
 import Catalogo from '../components/catalogo/Catalogo.js';
 
 import Favoritos from '../components/favoritos/Favoritos.js';
+import Shopping_cart from '../components/shopping_cart/Shopping_cart.js';
+
+import Busqueda from '../components/busqueda/Busqueda.js';
 
 function Router(){
     return(
@@ -18,6 +21,8 @@ function Router(){
                 <Route path='/register' element={<Register/>}/>
                 <Route path='/catalogo' element={<Catalogo/>}/>
                 <Route path='/favoritos' element={<Favoritos/>}/>
+                <Route path='/shopping_cart' element={<Shopping_cart/>}/>
+                <Route path='/accessories' element={<Busqueda/>}/>
             </Routes>
         </BrowserRouter>
     )
