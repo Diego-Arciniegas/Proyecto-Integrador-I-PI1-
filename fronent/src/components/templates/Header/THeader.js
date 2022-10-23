f6e850563fc47fee8eeb8f19be17b119bd244d77
diegomated <diegocardwr@gmail.com>
2022-10-23 17:13:48 -0500

    header template update

--- fronent/src/components/templates/Header/THeader.js
+++ fronent/src/components/templates/Header/THeader.js
@@ -1,11 +1,15 @@
 
 import './Header.css';
+import {useNavigate} from 'react-router-dom';
 
 function Header(){
+
+    const navigate = useNavigate();
+
     return(
         <header className="primary">
-            <img id="logo" src="./img/30.PNG" alt=""/>
-            <h2>CarParts & Accesories</h2>
+                <img id="logo" src="./img/30.PNG" alt=""/>
+                <div onClick={()=>{navigate('/catalogo')}}><h2>CarParts & Accesories</h2></div>
         </header>
     )
 }
