b601f57c29caefb43b2369fbed4b000c45a65669
diegomated <diegocardwr@gmail.com>
2022-10-23 09:31:09 -0500

    header template

---
+++ fronent/src/components/templates/Header/THeader.js
@@ -0,0 +1,13 @@
+
+import './Header.css';
+
+function Header(){
+    return(
+        <header className="primary">
+            <img id="logo" src="./img/30.PNG" alt=""/>
+            <h2>CarParts & Accesories</h2>
+        </header>
+    )
+}
+
+export default Header;
