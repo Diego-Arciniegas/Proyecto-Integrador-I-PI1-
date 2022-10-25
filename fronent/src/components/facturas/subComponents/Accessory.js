5da3fc9fdf1d39c4cf7fdf8a1e2bf313d5ce52d3
diegomated <diegocardwr@gmail.com>
2022-10-25 10:22:49 -0500

    facturas component

---
+++ fronent/src/components/facturas/subComponents/Accessory.js
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
+
+function Accessory(props){
+
+    const navigate = useNavigate();
+
+    const {id_order, user, total_price, quantity, date_creation} = props;
+
+    return(
+        <tr>
+            <td scope="row">{id_order}</td>
+            <td>{user.name_user}</td>
+            <td>${total_price}</td>
+            <td>{quantity}</td>
+            <td>{date_creation.split('T')[0]}</td>
+            <td>
+                <button onClick={()=>{navigate(`/admin/facturas/${id_order}`)}} type="button" className="btn btn-edit">factura</button>
+            </td>
+        </tr>
+    )
+}
+
+export default Accessory;
+
+
