0f4d3d07aba700de3f09b57a3c8299e7aa84b11f
diegomated <diegocardwr@gmail.com>
2022-10-25 10:25:32 -0500

    routes update

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -30,6 +30,8 @@ router.route('/users/:id_user/favorites/:id_accessory').delete(c.favorites.delet
 
 router.route('/accessories').get(c.accessories.getAccessories);
 router.route('/accessories').post(c.accessories.addAccessories);
+router.route('/accessories/price_history').get(c.accessories.getAccessoriesPriceHistory);
+router.route('/accessories/movement_history').get(c.accessories.getAccessoriesMovementHistory);
 router.route('/accessories/:id_accessory').get(c.accessories.getOneAccessory);
 router.route('/accessories/:id_accessory').put(c.accessories.editAccessory);
 router.route('/accessories/:id_accessory').delete(c.accessories.deleteAccessory);
@@ -37,6 +39,8 @@ router.route('/accessories/:id_accessory').delete(c.accessories.deleteAccessory)
 router.route('/orders').get(c.orders.getOrder);
 router.route('/orders/:id_order').get(c.orders.getOneOrder);
 router.route('/orders/:id_order').delete(c.orders.deleteOrder);
+
+router.route('/saved').get(c.accessories.savexd);
 
 module.exports = router;
 
