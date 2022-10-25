e95d9b0b237ac38606b217262e97c72fcd7c0992
diegomated <diegocardwr@gmail.com>
2022-10-25 10:20:03 -0500

    update component

--- fronent/src/components/home/Home.js
+++ fronent/src/components/home/Home.js
@@ -1,48 +1,20 @@
 import './Home.css';
 
-import TFooter from '../templates/Footer/TFooter.js';
+import TBusqueda from '../templates/Busqueda/TBusqueda.js';
 
 function Home(){
 
     return(
         <div>
-            <header className="primary">
-                <div className="col-5">
-                    <div className="dropdown">
-                        <button className="btn btn-secondary dropdown-toggle hamburger" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="./img/menu.png" alt=""/>
-                        </button>
-                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li><a className="dropdown-item">Action</a></li>
-                        <li><a className="dropdown-item">Another action</a></li>
-                        <li><a className="dropdown-item">Something else here</a></li>
-                        </ul>
-                    </div>
+            <TBusqueda/>
+            <div className="contenedor">
+                <img src="./img/28.jpeg" />
+                <div className="letras-portada text-start">
+                    <h1>SERVICIOS</h1>
+                    <h1>AUTOPARTES</h1>
+                    <h1>SOPORTE TECNICO</h1>
                 </div>
-                <div className="col-2 text-center">
-                    <img id="logo" src="./img/30.png" alt=""/>
-                </div>
-                <div className="col-5 d-flex">
-                    <div className="input-group d-flex justify-content-end ">
-                        <input  type="text" className="form-control rounded-pill" aria-label="Dollar amount (with dot and two decimal places)"/>
-                        <button id="buscar" className="input-group-text border-0">
-                            <img src="./img/lupa.png" alt=""/>
-                        </button>
-                    </div>
-                    <button id="profile" className="border-0">
-                        <img  src="./img/usuario.png" alt=""/>
-                    </button>
-                </div>
-        </header>
-
-        <div className="contenedor">
-            <img src="./img/28.jpeg" />
-            <div className="letras-portada text-start">
-                <h1>SERVICIOS</h1>
-                <h1>AUTOPARTES</h1>
-                <h1>SOPORTE TECNICO</h1>
             </div>
-        </div>
 
             <div className="row barra">
                 <div className="col-4 text-white menu-medio-start">
@@ -67,7 +39,6 @@ function Home(){
                 </div>
             </div>
             
-            <TFooter></TFooter>
         </div>
     )
 }
